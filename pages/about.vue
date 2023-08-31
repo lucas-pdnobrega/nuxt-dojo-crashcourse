a9decc47f89ad24d92251c9eb9476d63147152a4
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2023-08-31 10:56:41 -0300

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aperiam animi, corporis fugiat nobis aspernatur porro excepturi totam deleniti omnis illum ab saepe labore odit minima natus veritatis laboriosam? Fugiat?</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eos dolor quas tempora exercitationem. At, perferendis excepturi ducimus delectus quis eius repellat tempore eos quidem ullam illo inventore vitae libero.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae praesentium quisquam dicta? Error debitis iure omnis ullam fugiat cumque. Esse hic libero laudantium nam error at cupiditate totam temporibus perferendis!</p>
+        <p>EU NÃO VOU MAIS DEIXAR O PC ABERTO, OBRIGADO!</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
